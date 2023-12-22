0cf6c55e883dacf849fdcbc0fbdffaf77241482f
Filip Chalupa <chalupa.filip@gmail.com>
2023-12-22 15:26:05 +0100

    Base

---
+++ README.md
@@ -0,0 +1,25 @@
+# React ripple click [![npm](https://img.shields.io/npm/v/react-use-drag.svg)](https://www.npmjs.com/package/react-use-drag) ![npm type definitions](https://img.shields.io/npm/types/react-use-drag.svg)
+
+Drag interactions made easier. Try interactive [CodeSandbox demo](https://codesandbox.io/s/react-use-drag?file=/src/App.js).
+
+![UI example](https://raw.githubusercontent.com/FilipChalupa/react-use-drag/HEAD/screencast.gif)
+
+## Installation
+
+```bash
+npm install react-use-drag
+```
+
+## How to use
+
+```jsx
+import { useDrag } from 'react-use-drag'
+
+const App = () => {
+	const drag = useDrag()
+
+	// @TODO
+
+	return <button>Drag me</button>
+}
+```
---
+++ src/useDrag.ts
@@ -0,0 +1,82 @@
+import {
+	PointerEvent,
+	useCallback,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from 'react'
+
+export const useDrag = (
+	onRelativePositionChange: (x: number, y: number) => void,
+	onEnd: (x: number, y: number) => void,
+	onStart: () => void,
+) => {
+	const [isMoving, setIsMoving] = useState(false)
+	const startPosition = useRef({ x: 0, y: 0, scrollX: 0, scrollY: 0 })
+	const [offsetPosition, setOffsetPosition] = useState({ x: 0, y: 0 })
+
+	const onPointerDown = useCallback(
+		(event: PointerEvent<HTMLElement>) => {
+			event.preventDefault()
+			startPosition.current = {
+				x: event.clientX,
+				y: event.clientY,
+				scrollX: window.scrollX, // @TODO: handle any parent scroll
+				scrollY: window.scrollY, // @TODO: handle any parent scroll
+			}
+			event.currentTarget.setPointerCapture(event.pointerId)
+			setIsMoving(true)
+			onStart()
+		},
+		[onStart],
+	)
+
+	const onPointerUp = useMemo(() => {
+		if (!isMoving) {
+			return undefined
+		}
+		return (event: PointerEvent<HTMLElement>) => {
+			event.preventDefault()
+			setIsMoving(false)
+			event.currentTarget.releasePointerCapture(event.pointerId)
+			onEnd(offsetPosition.x, offsetPosition.y)
+			setOffsetPosition({ x: 0, y: 0 })
+		}
+	}, [isMoving, onEnd])
+
+	const onPointerMove = useMemo(() => {
+		if (!isMoving) {
+			return undefined
+		}
+		return (event: PointerEvent<HTMLElement>) => {
+			event.preventDefault()
+			const newOffsetPosition = {
+				x:
+					event.clientX +
+					window.scrollX -
+					(startPosition.current.x + startPosition.current.scrollX),
+				y:
+					event.clientY +
+					window.scrollY -
+					(startPosition.current.y + startPosition.current.scrollY),
+			}
+			setOffsetPosition(newOffsetPosition)
+		}
+	}, [isMoving])
+
+	useEffect(() => {
+		onRelativePositionChange(offsetPosition.x, offsetPosition.y)
+	}, [offsetPosition.x, offsetPosition.y, onRelativePositionChange])
+
+	const elementProps = useMemo(
+		() => ({
+			onPointerDown,
+			onPointerUp,
+			onPointerMove,
+		}),
+		[onPointerDown, onPointerMove, onPointerUp],
+	)
+
+	return useMemo(() => ({ isMoving, elementProps }), [isMoving, elementProps])
+}
